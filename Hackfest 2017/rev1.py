17d1beaa1d1683ab8e2bec86108220295a5d8a16
Rayen Messaoudi <psycoray4@gmail.com>
2017-03-28 00:04:51 +0100

    Create rev1.py

---
+++ Hackfest 2017/rev1.py
@@ -0,0 +1,34 @@
+#[HackFest Qualification 2017]
+#not the most elegent code :p
+
+def ff(x):
+	p = 1
+	while True:
+		s = p
+		p = p << 8
+		if p > x :
+			return s
+			break
+
+
+ch = '2GYhdiSLoJTRvASGXjIHtatb9Kdr'
+ch = ch[::-1]
+b = '123456789NOPQRSTUWXYZACDEFGHIJKLMnopqrstuvwxzabcdefghijklm'
+
+x = 0
+for i in range(len(ch))[::-1]:
+        m = b.find(ch[i])
+        x = x * 58 + m
+
+
+liste = []
+while x > 0 :
+	a , b = divmod(x,ff(x))
+	liste.append(a)
+	x = b
+
+r = ''
+for i in liste:
+	r+=chr(i)
+
+print "The flag is : ",r
